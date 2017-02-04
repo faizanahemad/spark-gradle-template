3260883c306b3da4b9ba41bb7c676a90fa81190d
faizanahemad <fahemad3@gmail.com>
2017-02-04 18:14:44 +0530

    Updating Main.scala

--- src/main/scala/template/spark/InitSpark.scala
+++ src/main/scala/template/spark/InitSpark.scala
@@ -4,8 +4,12 @@ import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql.SparkSession
 
 trait InitSpark {
-  val spark: SparkSession = SparkSession.builder().appName("Spark example").master("local[*]")
-                            .config("spark.some.config.option", "some-value").getOrCreate()
+  val spark: SparkSession = SparkSession.builder()
+                            .appName("Spark example")
+                            .master("local[*]")
+                            .config("spark.some.config.option", "some-value")
+                            .getOrCreate()
+
   val sc = spark.sparkContext
   val sqlContext = spark.sqlContext
   def reader = spark.read.option("header",true).option("inferSchema", true).option("mode", "DROPMALFORMED")
--- src/main/scala/template/spark/Main.scala
+++ src/main/scala/template/spark/Main.scala
@@ -9,11 +9,12 @@ object Main extends InitSpark {
     import spark.implicits._
 
     val version = spark.version
-    println("VERSION_STRING = " + version)
+    println("SPARK VERSION = " + version)
 
     val sumHundred = spark.range(1, 101).reduce(_ + _)
-    println(f"sum 1 to 100 = $sumHundred")
+    println(f"Sum 1 to 100 = $sumHundred")
 
+    println("Reading from csv file: people-example.csv")
     val persons = reader.csv("people-example.csv").as[Person]
     persons.show(2)
     val averageAge = persons.agg(avg("age")).first.get(0).asInstanceOf[Double]
